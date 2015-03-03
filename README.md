e77df5fb97000160c58cd79a20a35aecabcd5980
Will McGugan <willmcgugan@gmail.com>
2015-03-03 22:35:48 +0000

    fix

---
+++ README.md
@@ -0,0 +1,5 @@
+# Encrypted Notes
+
+A web application that stores encrypted notes.
+
+The encryption is done in the browser, and the password is never sent to the server -- so you don't need to trust the server owner.
--- site/wsgi.py
+++ site/wsgi.py
@@ -1,4 +1,5 @@
-# encoding=UTF-8from __future__ import unicode_literals
+# encoding=UTF-8
+from __future__ import unicode_literals
 from moya.wsgi import Application
 
 application = Application('./', ['local.ini', 'production.ini'], server='main', logging='prodlogging.ini')
