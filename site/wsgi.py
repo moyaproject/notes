f2df9a8ae0ffed46b4770158cb5e800dbe26d2cb
Will McGugan <willmcgugan@gmail.com>
2015-03-03 22:26:54 +0000

    wsgi fix

--- site/wsgi.py
+++ site/wsgi.py
@@ -1,4 +1,4 @@
 # encoding=UTF-8from __future__ import unicode_literals
 from moya.wsgi import Application
 
-application = Application('./', 'production.ini', server='main', logging='prodlogging.ini')
+application = Application('./', ['local.ini', 'production.ini'], server='main', logging='prodlogging.ini')
